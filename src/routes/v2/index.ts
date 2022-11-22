383fc1e3d8bfedd10db5848d8549db340ec2fc36
Cohen Erickson <cohenerickson@gmail.com>
2022-11-22 21:30:03 +0000

    Revert "Fix http protocol being rejected"
    This reverts commit d7a171fe1d1deaf527a6b770f61a3c2eb15a1087.

--- src/routes/v2/index.ts
+++ src/routes/v2/index.ts
@@ -4,16 +4,7 @@ export default async function Root(
   request: Request,
   server: any
 ): Promise<Response> {
-  const url = new URL(request.url);
-  if (/^\/v1\/ws-new-meta/.test(url.pathname)) {
-    // return await handleNewWsMeta(request);
-  } else if (/^\/v1\/ws-meta/.test(url.pathname)) {
-    // return await handleWsMeta(request);
-  } else if (request.headers.get("upgrade") === "websocket") {
-    // return await upgradeWsRequest(request, server);
-  } else {
-    // return await handleHttpRequest(request);
-  }
-
-  return new Response();
+  return new BareError(BareError.UNKNOWN, "unknown", {
+    message: "Not implemented."
+  });
 }
